1c42949747c1a1752c6a5ae4c966f7ec519795e5
@anusorn.krth <anusorn.krth@gmail.com>
2022-06-08 13:11:59 +0700

    07/06/65

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const upload = multer({
-    storage: multer.memoryStorage()
-})
 
 const userController = require('../Controllers/userController')
 
